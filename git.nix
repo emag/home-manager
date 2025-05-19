9ecd5a9a3263bde31fbb4396e84824117b5b41e8
emag <lanabe.lanabe@gmail.com>
2025-05-19 21:37:40 +0900

    Add git aliases

--- git.nix
+++ git.nix
@@ -3,5 +3,17 @@
     enable = true;
     userName = "emag";
     userEmail = "lanabe.lanabe@gmail.com";
+    aliases = {
+      adda = "add .";
+      cia = "commit -a -m";
+      co = "checkout";
+      d1 = "diff HEAD~";
+      di = "diff";
+      dc = "diff --cached";
+      ol = "log --oneline";
+      rsh = "reset --soft HEAD";
+      rsh1 = "reset --soft HEAD~";
+      st = "status";
+    };
   };
 }
